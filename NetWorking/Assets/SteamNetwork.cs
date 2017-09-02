741b0ae5057b9e16f1f93b5a74ecd2d7ade51c15
fangxm <1126716733@qq.com>
2017-09-02 13:09:05 +0800

    fangxm

--- NetWorking/Assets/SteamNetwork.cs
+++ NetWorking/Assets/SteamNetwork.cs
@@ -4,16 +4,23 @@ using System.Threading;
 using UnityEngine;
 using Steamworks;
 using System.Text;
+using UnityEngine.UI;
+
 public class SteamNetwork : MonoBehaviour {
 
-	// Use this for initialization
-	void Start ()
+    public Text SeverID;
+    public Text enter_text;
+    public InputField enter_input;
+    public Text chat_text;
+
+    // Use this for initialization
+    void Start ()
 	{
 	    print("注入Steam："+SteamAPI.Init());
 	   // InitTimer();
 
         //FindLobbies();
-	    CreateLobbies();
+	    //CreateLobbies();
 	}
 
     void Update()
@@ -22,8 +29,17 @@ public class SteamNetwork : MonoBehaviour {
 
     }
 
+    public void JoinSever()
+    {
+        FindLobbies();
+    }
 
-
+    public void EndEdit()
+    {
+        sendMsg(enter_text.text);
+        print(1);
+        enter_input.text = "";
+    }
 
     #region FindLobby
     CallResult<LobbyMatchList_t> CLobbyListManager = new CallResult<LobbyMatchList_t>();
@@ -36,12 +52,14 @@ public class SteamNetwork : MonoBehaviour {
     void OnLobbyMatchList(LobbyMatchList_t pCallback, bool bIOFailure)
     {
         print("找到服务器："+pCallback.m_nLobbiesMatching);
+        lobby = SteamMatchmaking.GetLobbyByIndex((int)pCallback.m_nLobbiesMatching-1);
+        JoinLobby();
     }
     #endregion
 
     #region CreateLobby
     CallResult<LobbyCreated_t> CLobbyCreator = new CallResult<LobbyCreated_t>();
-    void CreateLobbies()
+    public void CreateLobbies()
     {
         ELobbyType lobby_type = ELobbyType.k_ELobbyTypePublic;
 
@@ -65,17 +83,19 @@ public class SteamNetwork : MonoBehaviour {
     void JoinLobby()
     {
         var a = SteamMatchmaking.JoinLobby(lobby);
-        print(a.m_SteamAPICall+" ************");
+        //print(a.m_SteamAPICall+" ************");
         CLobbyJoin.Set(a, OnLobbyJoined);
     }
+
     void OnLobbyJoined(LobbyEnter_t pCallbacks, bool bIOFailure)
     {
         if (!bIOFailure)
         {
             print("--");
         }
-        print("加入服务器：" + bIOFailure);
-        print("服务器上锁" + pCallbacks);
+        print("服务器ID" + pCallbacks.m_ulSteamIDLobby);
+        print("服务器上锁：" + pCallbacks.m_bLocked);
+        SeverID.text = pCallbacks.m_ulSteamIDLobby.ToString();
         print(SteamMatchmaking.GetNumLobbyMembers(lobby));
         ChatInit();
         sendMsg("HelloWorld");
@@ -87,19 +107,20 @@ public class SteamNetwork : MonoBehaviour {
     void sendMsg(string t)
     {
         SteamMatchmaking.SendLobbyChatMsg(lobby, Encoding.Default.GetBytes(t), t.Length + 1);
+        chat_text.text = chat_text.text + t + "\n";
     }
 
     CallResult<LobbyChatMsg_t> CLobbyChat = new CallResult<LobbyChatMsg_t>();
     void ChatInit()
     {
-     CLobbyChat.Set(new SteamAPICall_t(LobbyChatMsg_t.k_iCallback),OnLobbyChatRecieved);
+        CLobbyChat.Set(new SteamAPICall_t(LobbyChatMsg_t.k_iCallback),OnLobbyChatRecieved);
         print("Chat system Done "+LobbyChatMsg_t.k_iCallback);
     }
 
     void OnLobbyChatRecieved(LobbyChatMsg_t pCallbacks, bool bIOFailure)
     {
-
+        print(2);
+        chat_text.text = chat_text.text + pCallbacks.ToString() + "\n";
         print("Chat:"+pCallbacks.m_eChatEntryType.ToString());
     }
-
 }
