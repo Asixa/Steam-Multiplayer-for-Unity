f973848d660561c56e7f0ae2bf4308ac8155e571
asixa <asixa@foxmail.com>
2017-11-03 18:36:11 +0800

    Asixa

---
+++ Assets/SteamMultiplayer/Demo/Script/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SteamMultiplayer;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class Player : SteamNetworkBehaviour
+{
+    public Platformer2DUserControl control;
+	// Use this for initialization
+	void Start () {
+	    if (!identity.IsLocalSpawned)
+	    {
+	        control.enabled = false;
+	    }
+	}
+	
+}
