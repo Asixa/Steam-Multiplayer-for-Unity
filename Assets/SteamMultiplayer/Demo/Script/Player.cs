a49934c0fc655837f9ea585e92af5b4ce5daf03f
asixa <asixa@foxmail.com>
2017-11-04 19:03:14 +0800

    Asixa

--- Assets/SteamMultiplayer/Demo/Script/Player.cs
+++ Assets/SteamMultiplayer/Demo/Script/Player.cs
@@ -7,12 +7,34 @@ using UnityStandardAssets._2D;
 public class Player : SteamNetworkBehaviour
 {
     public Platformer2DUserControl control;
+    public PlatformerCharacter2D chara;
+    public SpriteRenderer so_renderer;
+    public List<Color32> colors;
+
+    private int currentColor;
 	// Use this for initialization
 	void Start () {
+
 	    if (!identity.IsLocalSpawned)
 	    {
 	        control.enabled = false;
+	        chara.enabled = false;
 	    }
 	}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            rpcCall(0);
+        }
+    }
+
+    public void ChangeColor()
+    {
+        if (currentColor < 2) currentColor++;
+        else currentColor = 0;
+        so_renderer.color = colors[currentColor];
+    }
 	
 }
