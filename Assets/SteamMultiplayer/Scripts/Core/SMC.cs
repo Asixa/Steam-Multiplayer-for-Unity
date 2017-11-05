9ded23e623d592fc58236d21b5e90fc573226e25
asixa <asixa@foxmail.com>
2017-11-05 18:04:49 +0800

    Asixa

--- Assets/SteamMultiplayer/Scripts/Core/SMC.cs
+++ Assets/SteamMultiplayer/Scripts/Core/SMC.cs
@@ -109,7 +109,11 @@ namespace SteamMultiplayer
                 {
                     if (!t) ready = false;
                 }
-                if (ready) events.JoinedLobby.Invoke();
+                if (ready)
+                {
+                    events.JoinedLobby.Invoke();
+                    CheckingJoined = false;
+                }
             }
 
         }
@@ -186,8 +190,8 @@ namespace SteamMultiplayer
                     {
                         PlayerList.Add(steamid);
                         CreateConnection(steamid);
-                        PlayerNeedToConnect=new bool[PlayerList.Count];
-                        PlayerNeedToConnect[PlayerList.IndexOf(SelfID)] = true;
+                        //PlayerNeedToConnect=new bool[PlayerList.Count];
+                        //PlayerNeedToConnect[PlayerList.IndexOf(SelfID)] = true;
                     }
                     if (CheckingJoined)
                     {
